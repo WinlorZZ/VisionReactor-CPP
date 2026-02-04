219ef1457fdad135cac117b1649ee5d633a9532a
Equinn <571575141@qq.com>
2026-02-04 16:18:37 +0800

    在 Channel 类中新增获取和设置方法，增强对文件描述符和事件的管理

--- include/Channel.h
+++ include/Channel.h
@@ -18,11 +18,19 @@ public:
     void enableReading(); // 启用读事件监听，将事件添加到 epoll 实例中
     void handleEvent(); // 处理事件，调用相应的回调函数
 
+    int getFd() const { return fd; } // 获取文件描述符
+    uint32_t getEvents() const { return events; } // 获取关注的事件
+    Epoll* getEpoll() const { return ep; } // 获取关联的 Epoll 实例
+    bool isInEpoll() const { return isadd; } // 检查是否已添加到 Epoll 实例中
+    void setInEpoll(bool inEpoll) { isadd = inEpoll; } // 修改标记isadd
+    void setRevents(uint32_t revt) { revents = revt; } // 设置发生的事件
+
 private:
     int fd; // 文件描述符，每个channel对应唯一一个 fd
     Epoll* ep; // 指向 Epoll 实例的指针，用于注册和更新事件，每个 Channel 都关联一个 Epoll 实例
+    bool isadd = false; // 标记该 Channel 是否已添加到 Epoll 实例中，默认为 false未添加
     uint32_t events;  // 关注的事件 (用户设置的)
-    uint32_t revents; // 目前发生的事件 (epoll_wait 返回的)
+    uint32_t revents; // 目前发生的事件 (epoll_wait 返回的，由setRevents设置)
     std::function<void()> readCallback;  // 存储读回调函数
     std::function<void()> writeCallback; // 存储写回调函数
 };
