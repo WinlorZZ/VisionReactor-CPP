6fa13bb2dcd45f3749ef61e49280e6d0537d38ef
Equinn <571575141@qq.com>
2026-04-16 15:37:30 +0800

    【配置】 支持通过命令行参数配置AI目标地址。

--- main.cpp
+++ main.cpp
@@ -1,11 +1,17 @@
 #include "EventLoop.h"
 #include "Server.h"
 #include <signal.h>
+#include <string>
 
-int main(){
+int main(int argc, char* argv[]){
     //忽略 SIGPIPE 信号，防止往已关闭的 socket 写数据时程序崩溃
     signal(SIGPIPE, SIG_IGN);
-    
+    std::string ai_target = "127.0.0.1:50051"; // 默认值
+
+    // 如果从命令行传了参数，就覆盖默认值
+    if (argc >= 2) {
+        ai_target = argv[1]; 
+    }
     // 1. 创建事件循环 (主线程的心脏)
     EventLoop loop;
     // 2. 创建服务器 (拥有 Acceptor 和 ThreadPool)
